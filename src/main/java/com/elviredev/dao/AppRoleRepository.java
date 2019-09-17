12fc011d432dc671cb3e8d12a0cd63c438315e29
elviredev <sandrine.rodriguez@hotmail.com>
2019-09-17 12:00:59 +0200

    Interfaces DAO -> AppRoleRepository & AppUserRepository

---
+++ src/main/java/com/elviredev/dao/AppRoleRepository.java
@@ -0,0 +1,8 @@
+package com.elviredev.dao;
+
+        import com.elviredev.entities.AppRole;
+        import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppRoleRepository extends JpaRepository<AppRole, Long> {
+    public AppRole findByRoleName(String roleName);
+}
---
+++ src/main/java/com/elviredev/dao/AppUserRepository.java
@@ -0,0 +1,8 @@
+package com.elviredev.dao;
+
+import com.elviredev.entities.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    public AppUser findByUsername(String username);
+}
