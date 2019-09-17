d1c2fabf8986692d35875260ec6c1ec67424277a
elviredev <sandrine.rodriguez@hotmail.com>
2019-09-17 12:05:14 +0200

    Interfaces DAO -> ajout @RepositoryRestResource
    API REST pour la gestion des utilisateurs et des roles (en cas de besoin)

--- src/main/java/com/elviredev/dao/AppRoleRepository.java
+++ src/main/java/com/elviredev/dao/AppRoleRepository.java
@@ -2,7 +2,9 @@ package com.elviredev.dao;
 
         import com.elviredev.entities.AppRole;
         import org.springframework.data.jpa.repository.JpaRepository;
+        import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface AppRoleRepository extends JpaRepository<AppRole, Long> {
     public AppRole findByRoleName(String roleName);
 }
--- src/main/java/com/elviredev/dao/AppUserRepository.java
+++ src/main/java/com/elviredev/dao/AppUserRepository.java
@@ -2,7 +2,9 @@ package com.elviredev.dao;
 
 import com.elviredev.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     public AppUser findByUsername(String username);
 }
