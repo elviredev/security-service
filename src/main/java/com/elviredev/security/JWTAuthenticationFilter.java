030ac281c2a7e397bc6df73a5c60b0fab196f307
elviredev <sandrine.rodriguez@hotmail.com>
2019-09-17 17:57:16 +0200

    Update JWTAuthenticationFilter.java
    attemptAuthentication()

--- src/main/java/com/elviredev/security/JWTAuthenticationFilter.java
+++ src/main/java/com/elviredev/security/JWTAuthenticationFilter.java
@@ -1,8 +1,12 @@
 package com.elviredev.security;
 
+import com.elviredev.entities.AppUser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -14,17 +18,26 @@ import java.io.IOException;
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private AuthenticationManager authenticationManager;
 
+    // constructor
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
     }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+        // récupère username et pwd qui sont envoyées au format JSON avec library Jackson
+        try {
+            AppUser appUser = new ObjectMapper().readValue(request.getInputStream(), AppUser.class);
+            return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(appUser.getUsername(), appUser.getPassword()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+        // récupère utilisateur authentifié
+        User user = (User) authResult.getPrincipal(); // getPrincipal = user authentifié
     }
 }
