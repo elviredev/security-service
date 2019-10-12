e9a9c5f67ca06e7b54edd95fc4b78e4ee4a7a23c
elviredev <sandrine.rodriguez@hotmail.com>
2019-10-12 09:57:22 +0200

    Update JWTAuthenticationFilter.java

--- src/main/java/com/elviredev/security/JWTAuthenticationFilter.java
+++ src/main/java/com/elviredev/security/JWTAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.elviredev.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.elviredev.entities.AppUser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +16,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private AuthenticationManager authenticationManager;
@@ -39,5 +44,18 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
         // récupère utilisateur authentifié
         User user = (User) authResult.getPrincipal(); // getPrincipal = user authentifié
+        // déclarer une List de roles
+        List<String> roles = new ArrayList<>();
+        authResult.getAuthorities().forEach(auth -> {
+            roles.add(auth.getAuthority());
+        });
+
+        String jwt = JWT.create()
+                .withIssuer(request.getRequestURI()) // nom de l'autorité de l'application ayant généré le token
+                .withSubject(user.getUsername()) // nom de l'utilisateur
+                .withArrayClaim("roles", roles.toArray(new String[roles.size()])) // ajout des rôles
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityParams.EXPIRATION)) // date d'expiration
+                .sign(Algorithm.HMAC256(SecurityParams.SECRET)); // signature + secret
+        response.addHeader(SecurityParams.JWT_HEADER_NAME, jwt);
     }
 }
